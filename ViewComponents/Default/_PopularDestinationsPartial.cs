a5edfc8bdc77aa7f83ddf09a23976d069fd55d42
Sezer Sürücü <sezer@sezersurucu.com>
2022-02-11 11:29:17 +0300

    Layout Düzenlemesi => Layout Partiallere ayrıldı Index  => VievComponent'ler eklenerek Partial hale getirildi.

---
+++ ViewComponents/Default/_PopularDestinationsPartial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLAyer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.Default
+{
+    public class _PopularDestinationsPartial:ViewComponent
+    {
+        private DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+    }
+}
---
+++ ViewComponents/Default/_SliderPartial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.Default
+{
+    public class _SliderPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            //.net core ile gelen özellik .
+            //Partial ile View Component arasında ne far var ?
+            return View();
+        }
+    }
+}
